61377a9c2c5d3ab6fbbb1b5a961bff684989b893
alisoltanirad <soltaniradali@gmail.com>
2020-11-11 14:52:02 +0330

    code get_names function

--- gender_identifier.py
+++ gender_identifier.py
@@ -1,17 +1,23 @@
 # https://github.com/alisoltanirad/Gender-Identifier.git
 # Dependencies: nltk
-
-def classify_gender():
-    pass
-
-
-def get_names():
-    pass
-
+import random
+import nltk
 
 def main():
     names = get_names()
     classify_gender(names)
+
+
+def classify_gender(names):
+    pass
+
+
+def get_names():
+    nltk.download('names')
+    names = [(name, 'male') for name in nltk.corpus.names.words('male.txt')] + \
+            [(name, 'female') for name in nltk.corpus.names.words('female.txt')]
+    random.shuffle(names)
+    return names
 
 
 if __name__ == '__main__':
