2d95e9076a4e4e7646fa2d5c5412657f7106c97c
alisoltanirad <soltaniradali@gmail.com>
2020-11-11 14:59:23 +0330

    add download_corpus function

--- gender_identifier.py
+++ gender_identifier.py
@@ -1,5 +1,6 @@
 # https://github.com/alisoltanirad/Gender-Identifier.git
 # Dependencies: nltk
+import ssl
 import random
 import nltk
 
@@ -13,11 +14,16 @@ def classify_gender(names):
 
 
 def get_names():
-    nltk.download('names')
+    download_corpus()
     names = [(name, 'male') for name in nltk.corpus.names.words('male.txt')] + \
             [(name, 'female') for name in nltk.corpus.names.words('female.txt')]
     random.shuffle(names)
     return names
+
+
+def download_corpus():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    nltk.download('names')
 
 
 if __name__ == '__main__':
