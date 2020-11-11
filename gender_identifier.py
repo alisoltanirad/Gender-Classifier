d2966e5424f72d50430cdbbf81b23acfc050e97a
alisoltanirad <soltaniradali@gmail.com>
2020-11-11 15:27:28 +0330

    add split_corpus function

--- gender_identifier.py
+++ gender_identifier.py
@@ -10,7 +10,21 @@ def main():
 
 
 def classify_gender(names):
-    pass
+    data_set = [(extract_features(name), gender) for (name, gender) in names]
+    train_set, validation_set, test_set = split_corpus(data_set)
+
+
+def extract_features(name):
+    features = {}
+    return features
+
+
+def split_corpus(data_set):
+    test_set = data_set[-round(len(data_set)/5):]
+    train_validation = data_set[:-round(len(data_set)/5)]
+    train_set = train_validation[:-round(len(train_validation)/5)]
+    validation_set = train_validation[-round(len(train_validation)/5)]
+    return train_set, validation_set, test_set
 
 
 def get_names():
