3635d8d24824099b701d78ce2903ac084d725e83
alisoltanirad <soltaniradali@gmail.com>
2020-11-11 18:27:05 +0330

    delete show errors function

--- gender_identifier.py
+++ gender_identifier.py
@@ -12,41 +12,21 @@ def main():
 
 def classify_gender(names):
     data_set = [(extract_features(name), gender) for (name, gender) in names]
-    train_set, validation_set, test_set = split_corpus(data_set)
+    train_set, test_set = split_corpus(data_set)
 
     classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-    analyze_classifier(classifier, validation_set)
+    analyze_classifier(classifier)
     evaluate_classifier(classifier, test_set)
 
 
-def analyze_classifier(classifier, validation_set):
+def analyze_classifier(classifier):
     classifier.show_most_informative_features(50)
-    show_errors(classifier, validation_set)
-
-
-def show_errors(classifier, validation_set):
-    errors = get_errors(classifier, validation_set)
-    print('- Errors:')
-    for (gender, guess, features) in errors:
-        print(type(gender))
-        print('correct:{:<8} guess:{:<8} features:{}'.format(gender, guess,
-                                                             features))
-
-
-def get_errors(classifier, validation_set):
-    errors = []
-    for (features, gender) in validation_set:
-        guess = classifier.classify(features)
-        if guess != gender:
-            errors.append((gender, guess, features))
-
-    return errors
 
 
 def evaluate_classifier(classifier, test_set):
     accuracy = nltk.classify.accuracy(classifier, test_set)
-    print('Evaluation:\n- Accuracy: {:.2%}'.format(accuracy))
+    print('\nEvaluation\n\t- Accuracy: {:.2%}'.format(accuracy))
 
 
 def extract_features(name):
@@ -60,11 +40,9 @@ def extract_features(name):
 
 
 def split_corpus(data_set):
-    test_set = data_set[-round(len(data_set)/5):]
-    train_validation = data_set[:-round(len(data_set)/5)]
-    train_set = train_validation[:-round(len(train_validation)/5)]
-    validation_set = train_validation[-round(len(train_validation)/5):]
-    return train_set, validation_set, test_set
+    train_set = data_set[:-round(len(data_set)/4)]
+    test_set = data_set[-round(len(data_set)/4):]
+    return train_set, test_set
 
 
 def get_names():
